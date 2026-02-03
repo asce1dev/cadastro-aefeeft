c5a91b9a6c8ace81f7232ef454313b6d04925254
asce1dev <alandelonwk@gmail.com>
2026-02-03 16:31:59 -0300

    Configuração do CORS e Inicio na criação de Testes Unitários

---
+++ src/main/java/com/asce1dev/cadastroaefeeft/core/web/WebConfig.java
@@ -0,0 +1,15 @@
+package com.asce1dev.cadastroaefeeft.core.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
+}
---
+++ src/test/java/com/asce1dev/cadastroaefeeft/domain/service/ClienteServiceTest.java
@@ -0,0 +1,113 @@
+package com.asce1dev.cadastroaefeeft.domain.service;
+
+import com.asce1dev.cadastroaefeeft.domain.model.Cliente;
+import com.asce1dev.cadastroaefeeft.domain.repository.ClienteRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+class ClienteServiceTest {
+
+    @Mock
+    private ClienteRepository clienteRepository;
+
+    @InjectMocks
+    private ClienteService clienteService;
+
+    @Test
+    void deve_listar_todos_quando_nome_e_cpf_vazios() {
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<Cliente> pageMock = new PageImpl<>(List.of(), pageable, 0);
+
+        when(clienteRepository.findAll(pageable)).thenReturn(pageMock);
+
+        Page<Cliente> result = clienteService.listarClientes(null, null, pageable);
+
+        assertNotNull(result);
+        verify(clienteRepository).findAll(pageable);
+        verifyNoMoreInteractions(clienteRepository);
+    }
+
+    @Test
+    void deve_buscar_clientes_por_nome_quando_informado() {
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<Cliente> pageMock = new PageImpl<>(List.of(), pageable, 0);
+
+        when(clienteRepository.findClienteByNomeStartingWithIgnoreCase("Al", pageable)).thenReturn(pageMock);
+
+        Page<Cliente> result = clienteService.listarClientes("  Al  ", null, pageable);
+
+        assertNotNull(result);
+        verify(clienteRepository, times(1))
+                .findClienteByNomeStartingWithIgnoreCase("Al", pageable);
+        verify(clienteRepository, never()).findAll(any(Pageable.class));
+        verifyNoMoreInteractions(clienteRepository);
+    }
+
+    @Test
+    void deve_buscar_clientes_por_cpf_removendo_mascara() {
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<Cliente> pageMock = new PageImpl<>(List.of(), pageable, 0);
+
+        when(clienteRepository.findClienteByCpfStartingWith("234109", pageable)).thenReturn(pageMock);
+
+        Page<Cliente> result = clienteService.listarClientes(null, " 234.109 ", pageable);
+
+        assertNotNull(result);
+        verify(clienteRepository, times(1))
+                .findClienteByCpfStartingWith("234109", pageable);
+        verify(clienteRepository, never()).findAll(any(Pageable.class));
+        verifyNoMoreInteractions(clienteRepository);
+    }
+
+    @Test
+    void deve_salvar_cliente_com_sucesso() {
+        Cliente cliente = new Cliente();
+
+        when(clienteRepository.saveAndFlush(cliente)).thenReturn(cliente);
+
+        Cliente result = clienteService.salvarCliente(cliente);
+
+        assertNotNull(result);
+        verify(clienteRepository).saveAndFlush(cliente);
+        verifyNoMoreInteractions(clienteRepository);
+    }
+
+    @Test
+    void deve_deletar_cliente_com_sucesso() {
+        Long id = 1L;
+
+        clienteService.deletarCliente(id);
+
+        verify(clienteRepository).deleteById(id);
+        verifyNoMoreInteractions(clienteRepository);
+    }
+
+    @Test
+    void deve_buscar_cliente_quando_id_existir() {
+        Cliente cliente = new Cliente();
+        Long id = 1L;
+
+        when(clienteRepository.findById(id)).thenReturn(Optional.of(cliente));
+
+        Cliente result = clienteService.buscarOuFalhar(id);
+
+        assertNotNull(result);
+        verify(clienteRepository).findById(id);
+        verifyNoMoreInteractions(clienteRepository);
+    }
+}
