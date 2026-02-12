4dc5d6b5fd1dc8c818124b54d647af34942ddb66
asce1dev <alandelonwk@gmail.com>
2026-02-12 16:56:01 -0300

    Novos testes unitários adicionados

--- src/test/java/com/asce1dev/cadastroaefeeft/domain/service/ClienteServiceTest.java
+++ src/test/java/com/asce1dev/cadastroaefeeft/domain/service/ClienteServiceTest.java
@@ -1,5 +1,9 @@
 package com.asce1dev.cadastroaefeeft.domain.service;
 
+import com.asce1dev.cadastroaefeeft.domain.exception.ClienteNaoEncontradoException;
+import com.asce1dev.cadastroaefeeft.domain.exception.CpfDuplicadoException;
+import com.asce1dev.cadastroaefeeft.domain.exception.EntidadeEmUsoException;
+import com.asce1dev.cadastroaefeeft.domain.exception.NegocioException;
 import com.asce1dev.cadastroaefeeft.domain.model.Cliente;
 import com.asce1dev.cadastroaefeeft.domain.repository.ClienteRepository;
 import org.junit.jupiter.api.Test;
@@ -7,6 +11,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -16,6 +22,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 
@@ -75,6 +82,16 @@ class ClienteServiceTest {
     }
 
     @Test
+    void deve_acusar_erro_com_nome_e_cpf() {
+        Pageable pageable = PageRequest.of(0, 10);
+
+        assertThrows(NegocioException.class, () ->
+                clienteService.listarClientes(" Al ", " 234.109", pageable));
+
+        verifyNoInteractions(clienteRepository);
+    }
+
+    @Test
     void deve_salvar_cliente_com_sucesso() {
         Cliente cliente = new Cliente();
 
@@ -88,6 +105,19 @@ class ClienteServiceTest {
     }
 
     @Test
+    void deve_acusar_erro_de_cpf_duplicado() {
+        Cliente cliente = new Cliente();
+
+        when(clienteRepository.saveAndFlush(cliente))
+                .thenThrow(DataIntegrityViolationException.class);
+
+        assertThrows(CpfDuplicadoException.class, () ->
+                clienteService.salvarCliente(cliente));
+
+        verify(clienteRepository).saveAndFlush(cliente);
+    }
+
+    @Test
     void deve_deletar_cliente_com_sucesso() {
         Long id = 1L;
 
@@ -95,6 +125,34 @@ class ClienteServiceTest {
 
         verify(clienteRepository).deleteById(id);
         verifyNoMoreInteractions(clienteRepository);
+    }
+
+    @Test
+    void deve_acusar_erro_de_cliente_nao_encontrado() {
+        Long id = 1L;
+
+        doThrow(new EmptyResultDataAccessException(1))
+                .when(clienteRepository)
+                .deleteById(id);
+
+        assertThrows(ClienteNaoEncontradoException.class, () ->
+                clienteService.deletarCliente(id));
+
+        verify(clienteRepository).deleteById(id);
+    }
+
+    @Test
+    void deve_acusar_erro_de_cliente_em_uso() {
+        Long id = 1L;
+
+        doThrow(new DataIntegrityViolationException(""))
+                .when(clienteRepository)
+                .deleteById(id);
+
+        assertThrows(EntidadeEmUsoException.class, () ->
+                clienteService.deletarCliente(id));
+
+        verify(clienteRepository).deleteById(id);
     }
 
     @Test
@@ -109,5 +167,17 @@ class ClienteServiceTest {
         assertNotNull(result);
         verify(clienteRepository).findById(id);
         verifyNoMoreInteractions(clienteRepository);
+    }
+
+    @Test
+    void deve_acusar_erro_de_cliente_nao_encontrado_ao_tentar_buscar() {
+        Long id = 1L;
+
+        when(clienteRepository.findById(id)).thenReturn(Optional.empty());
+
+        assertThrows(ClienteNaoEncontradoException.class, () ->
+                clienteService.buscarOuFalhar(id));
+
+        verify(clienteRepository).findById(id);
     }
 }
